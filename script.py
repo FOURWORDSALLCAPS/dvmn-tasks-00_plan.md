4de0a1ba61adb399027f126e9e179c54cc1b6e26
FOURWORDSALLCAPS <48273739+FOURWORDSALLCAPS@users.noreply.github.com>
2023-04-27 09:14:56 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,6 @@
 import requests
 
-<<<<<<< HEAD
 citys = [
-=======
-city = [
->>>>>>> b7eeaab40fddb58db1a8ff03796273db23b4cf62
 	'Череповец', 	
 	'Лондон', 
 	'Шереметьево'
@@ -15,12 +11,7 @@ payload = {
 	'lang': 'ru',
 }
 	
-<<<<<<< HEAD
 for city in citys:
 	response = requests.get(f'https://wttr.in/{city}', params=payload)
-=======
-for city_templates in city:
-	response = requests.get(f'https://wttr.in/{city_templates}', params=payload)
->>>>>>> b7eeaab40fddb58db1a8ff03796273db23b4cf62
 	response.raise_for_status()
 	print(response.text)
